5bd0e0bdc04d6f4d520550ec7d3d8d0ffb906028
Nguyen Minh Phung <nguyenminhphung1307@gmail.com>
2026-06-04 20:32:35 +0700

    chore: remove unused implementation plan document

--- backend/routes/index.route.js
+++
@@ -1,6 +0,0 @@
-const route = require("express").Router();
-const usersRoute = require("./users.route");
-
-route.use("/users", usersRoute);
-
-module.exports = route;
--- backend/routes/users.route.js
+++
@@ -1,7 +0,0 @@
-const route = require("express").Router();
-const usersController = require("../controllers/users.controller");
-
-route.post("/login", usersController.loginController);
-route.post("/register", usersController.registerController);
-route.get("/profile", usersController.profileController);
-module.exports = route;
